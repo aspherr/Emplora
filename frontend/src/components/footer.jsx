f48973e5945f77b64bacf742c90a8e1456d0924f
aspherr <huzaifa.syedd@gmail.com>
2025-07-28 20:21:47 -0400

    reduced footer opacity

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 pb-2 w-full h-15 flex items-center justify-center font-mono text-xs text-zinc-700 bg-base-200">
+        <footer className="fixed bottom-0 pb-2 w-full h-15 flex items-center justify-center font-mono text-xs text-zinc-700 bg-base-200 opacity-50">
             <span>Emplora © 2025</span>
         </footer>
     );
