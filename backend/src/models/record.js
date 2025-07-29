dce9f3e4bdd73513375ea307a2f40f86c7b38094
aspherr <huzaifa.syedd@gmail.com>
2025-07-28 21:43:53 -0400

    created 'record' model

---
+++ backend/src/models/record.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const recordSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: false
+    },
+
+    department: {
+        type: String,
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    managerID: {
+        type: String,
+        required: false
+    },
+
+    status: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true} );
+
+const Record = mongoose.model("Record", recordSchema);
+
+export default Record;
