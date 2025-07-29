be732273c5292352c87bc2d216478e14dd76209c
aspherr <huzaifa.syedd@gmail.com>
2025-07-29 18:31:43 -0400

    implemented rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,24 +2,22 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 
-import recordRoutes from "./routes/recordsRoutes.js" 
+import recordRoutes from "./routes/recordsRoutes.js"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
-
 // middleware
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log(`Request method: ${req.method} | Request URL: ${req.url}`);
-    next();
-})
+app.use(rateLimiter);
 
 app.use("/api/records", recordRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started at port: 3000")
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server started at port: ${PORT}`)
+    });
 });
