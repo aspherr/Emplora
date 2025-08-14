5f1b8bc5b954673d3f0e31f9e168d0a278d1c636
aspherr <huzaifa.syedd@gmail.com>
2025-08-14 00:01:40 -0400

    fixed missing corresponding colours for employee status

--- frontend/src/components/status.jsx
+++ frontend/src/components/status.jsx
@@ -1,11 +1,11 @@
 const status = ({record}) => {
   return (
     <div className='≈ bg-base-200 rounded item-center justify-center p-2 mt-2'>
-        <span className={` ${ record.status === 'active'
+        <span className={` ${ record.status === 'Active'
         ? 'text-green-600'
-        : record.status === 'on leave'
+        : record.status === 'On-Leave'
         ? 'text-yellow-500'
-        : record.status === 'terminated'
+        : record.status === 'Terminated'
         ? 'text-red-600'
         : 'text-gray-500'
         }`}>
